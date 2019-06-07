649268f9fe2d6e34239eb8e2d12ec9d91959a0b0
Galomortal47 <44421928+Galomortal47@users.noreply.github.com>
2019-06-07 13:35:53 -0300

    torre e guardas
    torre e guardas

---
+++ Lvls/Victory.gd
@@ -0,0 +1,10 @@
+extends Area
+
+func _on_Victory_area_entered(area):
+	get_tree().change_scene("res://Ui/victory.tscn")
+	pass # Replace with function body.
+
+
+func _on_Victory_body_entered(body):
+	get_tree().change_scene("res://Ui/victory.tscn")
+	pass # Replace with function body.
--- Ui/Button.gd
+++ Ui/Button.gd
@@ -1,7 +1,7 @@
 extends Button
 
 func _on_Button_mouse_entered():
-	get_parent().set_scale(Vector2(1.15,1.15))
+	get_parent().set_scale(Vector2(1.05,1.05))
 	pass # Replace with function body.
 
 func _on_Button_mouse_exited():
